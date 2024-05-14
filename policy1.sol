9336cc7e062abec1651177b9ed26e294c1c30f08
Wanjiru <109018412+MurdochK@users.noreply.github.com>
2024-05-14 22:48:56 +0300

    Create policy1.sol

---
+++ policy1.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract CarInsurance is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _policyIds;
+
+    struct PolicyDetails {
+        uint256 policyId;
+        address insured;
+        uint256 age;
+        uint256 premiumAmount;
+        uint256 averageSpeed;
+        string drivingLocation;
+        bool hasTelematics;
+        bool drinkingHistory;
+        bool healthStatus;
+        bool isActuary;
+    }
+
+    mapping(uint256 => PolicyDetails) public policies;
+
+    function createPolicy(
+        address _insured,
+        uint256 _age,
+        uint256 _premiumAmount,
+        uint256 _averageSpeed,
+        string memory _drivingLocation,
+        bool _hasTelematics,
+        bool _drinkingHistory,
+        bool _healthStatus,
+        bool _isActuary
+    ) public onlyOwner {
+        _policyIds.increment();
+        uint256 policyId = _policyIds.current();
+
+        PolicyDetails memory policyDetails = PolicyDetails(
+            policyId,
+            _insured,
+            _age,
+            _premiumAmount,
+            _averageSpeed,
+            _drivingLocation,
+            _hasTelematics,
+            _drinkingHistory,
+            _healthStatus,
+            _isActuary
+        );
+
+        policies[policyId] = policyDetails;
+    }
+
+    function getPolicy(uint256 _policyId) public view returns (PolicyDetails memory) {
+        return policies[_policyId];
+    }
+}
